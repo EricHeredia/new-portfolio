935e46f037d3daa9a6f03b14a57fab0c4ecfbbce
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-11 15:44:57 -0500

    centered links on hover

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,8 +6,10 @@ const Project = (props) => {
     <div className="project">
       <h2>{props.name}</h2>
       <img src={props.image} alt="test" />
-      <a href="google.com">Link1</a>
-      <a href="google.com">Link2</a>
+      <div className="project-links vCenter">
+        <a href="google.com">Link1</a>
+        <a href="google.com">Link2</a>
+      </div>
     </div>
   )
 }
