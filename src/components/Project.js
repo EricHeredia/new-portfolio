cb45704ba0c885d815414b997c94b379dba61b90
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-11 15:57:26 -0500

    finished styling links

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './Project.scss'
 
 const Project = (props) => {
+  //<i class="fab fa-codepen"></i>
   return (
     <div className="project">
       <h2>{props.name}</h2>
       <img src={props.image} alt="test" />
       <div className="project-links vCenter">
-        <a href="google.com">Link1</a>
-        <a href="google.com">Link2</a>
+        <a href="google.com" className="hover-link" title="View Project"><i class="fas fa-eye"></i></a>
+        <a href="google.com" className="hover-link" title="View Code"><i class="fab fa-github"></i></a>
       </div>
     </div>
   )
