ba0f5279190dc4800dfcc679fe6df76ce1f0e0ef
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-11 02:33:25 -0500

    added text to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,15 @@ import './Contact.scss'
 const Contact = () => {
   return (
     <div id="contact">
-      <p>Contact.</p>
+      <h2 className="large-head">Contact</h2>
+      <p>Feel free to send me an E-mail at</p>
+      <p className="email-text">EricDavidHeredia@gmail.com</p>
+      <br />
+      <p className="connect">Or connect via</p>
+      <br />
+      <div className="link-git">
+        <i className="fab fa-linkedin lower"></i>||<i className="fab fa-github-square lower"></i>
+      </div>
     </div>
   )
 }
